87e7ee70945efb73b9dbfc9cb3306ebbb3ccb855
Pedro Palugan <88800549+pedropalugan@users.noreply.github.com>
2022-02-17 14:58:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
 const Long = () => {
-    let min = 25;
+    let div = document.getElementById("mainContent");
+    div.parentNode.removeChild(div);
+    document.getElementById("main").innerHTML = ` 
+    <div id = "mainContent">
+    <button onclick = "StartL()">Start</button>
+    <button onclick = "Long()">Long Break</button>
+    <button onclick = "Medium()">Medium Break</button>
+    <button onclick = "Short()">Short Break</button>
+    <output id = "timer"></output>
+</div>
+    `
 }
 
 const Medium = () => {
-    let min = 15;
+    let div = document.getElementById("mainContent");
+    div.parentNode.removeChild(div);
+    document.getElementById("main").innerHTML = ` 
+    <div id = "mainContent">
+    <button onclick = "StartS()">Start</button>
+    <button onclick = "Long()">Long Break</button>
+    <button onclick = "Medium()">Medium Break</button>
+    <button onclick = "Short()">Short Break</button>
+    <output id = "timer"></output>
+</div>
+    `
 }
 
 const Short = () => {
-    let min = 5;
+    let div = document.getElementById("mainContent");
+    div.parentNode.removeChild(div);
+    document.getElementById("main").innerHTML = ` 
+    <div id = "mainContent">
+    <button onclick = "StartS()">Start</button>
+    <button onclick = "Long()">Long Break</button>
+    <button onclick = "Medium()">Medium Break</button>
+    <button onclick = "Short()">Short Break</button>
+    <output id = "timer"></output>
+</div>
+    `
+}
+
+
+function StartL(){
+    let min = 24;
+    let sec = 59;
 }
