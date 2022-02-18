ae2b4f11453c104c725321a87187afd06ad854ae
Pedro Palugan <88800549+pedropalugan@users.noreply.github.com>
2022-02-18 15:42:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const Long = () => {
     div.parentNode.removeChild(div);
     document.getElementById("main").innerHTML = ` 
     <div id = "mainContent">
-    <output id = "timer">25:00</output>
+    <output id = "timer" class = "timer">25:00</output>
+    <div class="box2">
     <button onclick = "StartL()" class="btn btn-success">Start</button>
     <button onclick = "Long()" class="btn btn-primary">Long Break</button>
     <button onclick = "Medium()" class="btn btn-primary">Medium Break</button>
     <button onclick = "Short()" class="btn btn-primary">Short Break</button>
-</div>
+    </div>
+    </div>
     `
 }
 
@@ -17,12 +19,14 @@ const Medium = () => {
     div.parentNode.removeChild(div);
     document.getElementById("main").innerHTML = ` 
     <div id = "mainContent">
-    <output id = "timer">15:00</output>
+    <output id = "timer" class = "timer">15:00</output>
+    <div class="box2">
     <button onclick = "StartM()" class="btn btn-success">Start</button>
     <button onclick = "Long()" class="btn btn-primary">Long Break</button>
     <button onclick = "Medium()" class="btn btn-primary">Medium Break</button>
     <button onclick = "Short()" class="btn btn-primary">Short Break</button>
-</div>
+    </div>
+    </div>
     `
 }
 
@@ -30,12 +34,14 @@ const Short = () => {
     let div = document.getElementById("mainContent");
     div.parentNode.removeChild(div);
     document.getElementById("main").innerHTML = ` 
-    <div id = "mainContent">
-    <output id = "timer">5:00</output>
+<div id = "mainContent">
+    <output id = "timer" class = "timer">5:00</output>
+    <div class="box2">
     <button onclick = "StartS()" class="btn btn-success">Start</button>
     <button onclick = "Long()" class="btn btn-primary">Long Break</button>
     <button onclick = "Medium()" class="btn btn-primary">Medium Break</button>
     <button onclick = "Short()" class="btn btn-primary">Short Break</button>
+    </div>
 </div>
     `
 }
@@ -70,20 +76,21 @@ function StartL(){
             }
             else if(min == 0){
                 if (sec < 10){
+                    sec--
                     document.getElementById("timer").innerHTML = min + " : " + "0"+sec;
-                sec--
+                
                     }
                     else{
-                        document.getElementById("timer").innerHTML = min + " : " + sec; 
                         sec--
+                        document.getElementById("timer").innerHTML = min + " : " + sec; 
                     }
             }
         }
     }
 
     if (min > 0){
-        setInterval(timerLS, 1000);
-        setInterval(timerLM, 60000);
+        setInterval(timerLS, 10);
+        setInterval(timerLM, 600);
         }
 
     }
@@ -117,20 +124,21 @@ function StartL(){
                 }
                 else if(min == 0){
                     if (sec < 10){
+                        sec--
                         document.getElementById("timer").innerHTML = min + " : " + "0"+sec;
-                    sec--
                         }
                         else{
-                            document.getElementById("timer").innerHTML = min + " : " + sec; 
                             sec--
+                            document.getElementById("timer").innerHTML = min + " : " + sec; 
+                            
                         }
                 }
             }
         }
     
         if (min > 0){
-            setInterval(timerLS, 1000);
-            setInterval(timerLM, 60000);
+            setInterval(timerLS, 10);
+            setInterval(timerLM, 600);
             }
     
         }
@@ -165,20 +173,22 @@ function StartL(){
                     }
                     else if(min == 0){
                         if (sec < 10){
+                            sec--
                             document.getElementById("timer").innerHTML = min + " : " + "0"+sec;
-                        sec--
+                        
                             }
                             else{
-                                document.getElementById("timer").innerHTML = min + " : " + sec; 
                                 sec--
+                                document.getElementById("timer").innerHTML = min + " : " + sec; 
+                            
                             }
                     }
                 }
             }
         
             if (min > 0){
-                setInterval(timerLS, 1000);
-                setInterval(timerLM, 60000);
+                setInterval(timerLS, 10);
+                setInterval(timerLM, 600);
                 }
         
             }
